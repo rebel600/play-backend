cdd5842d7b2c9c998711f35a3205936ad956730d
rebel@600 <shyampareek232@gmail.com>
2023-11-10 22:26:21 +0530

    video and user schema added with validation check using jwt and bcrypt

--- src/utils/ApiResponse.js
+++ src/utils/ApiResponse.js
@@ -1,15 +1,9 @@
-class ApiResponse extends Error {
-  constructor(statusCode, message = "Success", data, stack = "") {
+class ApiResponse {
+  constructor(statusCode, message = "Success", data) {
     this.statusCode = statusCode;
     this.message = message;
     this.data = data;
     this.success = statusCode < 400;
-
-    if (stack) {
-      this.stack = stack;
-    } else {
-      Error.captureStackTrace(this, this.constructor);
-    }
   }
 }
 
