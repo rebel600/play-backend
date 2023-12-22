8f6487c91717672c53d20cd1946880fb4f735a17
rebel@600 <shyampareek232@gmail.com>
2023-12-22 23:46:36 +0530

    "user controller and routes added!"

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import cors from "cors";
 import express from "express";
 import cookieParser from "cookie-parser";
+import userRouter from "./routes/user.routes.js";
 
 const app = express();
 
@@ -12,8 +13,13 @@ app.use(
 );
 
 app.use(express.json({ limit: "16kb" }));
-app.use(express.urlencoded({ exptended: true, limit: "16kb" }));
+// app.use(express.urlencoded({ exptended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
+
+// routes import
+
+// routes declaration
+app.use("/api/v1/users", userRouter);
 
 export { app };
