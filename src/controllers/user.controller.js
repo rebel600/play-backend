812c42514a5712fb402b2cd89b33fe66d8017b4e
rebel@600 <shyampareek232@gmail.com>
2023-12-23 00:00:33 +0530

    all good

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -2,7 +2,7 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 
 const registerUser = asyncHandler(async (req, res) => {
   res.status(201).json({
-    message: "created",
+    message: "created!",
   });
 });
 const loginUser = asyncHandler(async (req, res) => {
